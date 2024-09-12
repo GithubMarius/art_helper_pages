805947daf5688722d6db7d4512b697b74c5dc8e0
Marius Hofmann <mariushofmann@yahoo.de>
2024-09-12 11:58:56 +0200

    Fixing base url.

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/art_helper_pages/sw.js', { scope: '/art_helper_pages/' })})}
